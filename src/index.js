9df23a904fa42cfa201e7fd2c63f15cbeda71b96
Thangtea <riomiyachi.1991@gmail.com>
2019-01-10 08:34:46 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import App from './App';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
+
+const store=createStore(rootReducer);
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
